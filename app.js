ce6b46b15ea27aee90a2bbe154b5eca4dd79cce0
Scott Lai <tuonglai3602@gmail.com>
2021-04-03 14:55:14 -0700

    Added admin.

--- app.js
+++ app.js
@@ -11,17 +11,20 @@ const app = express();
 const quizzesRouter = require('./src/routes/v0/quizzes');
 const questionsRouter = require('./src/routes/v0/questions');
 const submissionRouter = require('./src/routes/v0/submissions');
+const adminRouter = require('./src/routes/v0/admin');
 
 // Log requests to the console.
 app.use(logger('dev'));
 
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.set('view engine', 'ejs');
+
 app.use('/api/v0/quizzes', quizzesRouter);
 app.use('/api/v0/questions', questionsRouter);
 app.use('/api/v0/submission', submissionRouter);
+app.use('/api/v0/admin', adminRouter);
 
 module.exports = app;
